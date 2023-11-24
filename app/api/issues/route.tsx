4ce4e99646448be5423c2ed924432bd887ab6d10
Sami Bhojani <msammi96@gmail.com>
2023-11-24 19:42:30 +0500

    Correction in Create Issue API

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -3,7 +3,7 @@ import { z } from 'zod';
 import prisma from "@/prisma/client";
 
 const createIssueSchema = z.object ({
-    title: z.string().min(1).max(255)
+    title: z.string().min(1).max(255),
     description: z.string().min(1)
 })
 
