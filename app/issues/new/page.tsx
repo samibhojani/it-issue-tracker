4d2f43baad1d7b40e2325a559f6c1b5240f81877
Sami Bhojani <msammi96@gmail.com>
2023-11-24 22:59:01 +0500

    Form setup react-hook-form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,19 +1,33 @@
 "use client"
 
 import { TextField, Button } from '@radix-ui/themes'
-import SimpleMDE from "react-simplemde-editor";
+import SimpleMDE from 'react-simplemde-editor';
+import { useForm, Controller,  } from 'react-hook-form'
 import "easymde/dist/easymde.min.css";
 
-const NewIssuePage = () => {
+interface issueForm {
+    title: string;
+    description: string;
+}
+
+
+const NewIssuePage =  ()=> {
+    const {register, control,handleSubmit} = useForm <issueForm>();
+    
   return (
     <>
-    <div className='max-w-xl space-y-3'>
+    <form className='max-w-xl space-y-3' 
+    onSubmit={handleSubmit((data) => console.log(data))}>
       <TextField.Root>
-        <TextField.Input placeholder='Issue Title'/>
+        <TextField.Input placeholder='Issue Title' {...register('title')}/>
       </TextField.Root>
-      <SimpleMDE placeholder='Issue Description'/>
+        <Controller 
+        name='description'
+        control={control}
+        render={({ field }) =>  <SimpleMDE placeholder='Issue Description' {...field }/>}
+        />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
     </>
   )
 }
